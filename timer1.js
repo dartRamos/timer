ae83a8fc2963b1050655def28893c7a420542c45
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-17 18:19:54 -0500

    fixed issues using eslint

--- timer1.js
+++ timer1.js
@@ -6,31 +6,20 @@ const alarms = [];
 args.forEach(arg => {
   const num = Number(arg); // convers argument into  a number
 
-  if (!isNaN(num) && num > 0) {
-    alarms.push(num)
-  };
+  if (!isNaN(num) && num > 0) { // checks if its a number and is positive. If so pushes it into alarms array
+    alarms.push(num);
+  }
 });
 
-if(alarms.length === 0) {
+if (alarms.length === 0) { // if nothing is pushed into alarms the program stops
   process.exit();
-};
+}
 
 alarms.forEach((timer) => {
   setTimeout(() => {
-    process.stdout.write("\x07");
-    console.log(`Alarm set for ${timer} seconds!`)
-  }, timer * 1000);
+    process.stdout.write("\x07"); // sets a sounds for timer
+    console.log(`Alarm for ${timer} seconds!`);
+  }, timer * 1000); // converts seconds into milliseconds
 });
 
-console.log(`These are the alarms in seconds: ${alarms}`)
-
-
-// console.log("Alarm set:", args);
-
-
-
-
-
-
-
-// process.stdout.write("\x07");
+console.log(`These are the alarms in seconds: ${alarms}`);
